2ed295fab40c2ba03db84133a042ff11a1a22c2e
nishibu2003 <nishibu2003@gmail.com>
2025-08-15 22:33:49 +0900

    Add FileUtil.truncateForDir

--- src/main/java/com/nishibu2003/m3u8_downloader/file/FileUtil.java
+++ src/main/java/com/nishibu2003/m3u8_downloader/file/FileUtil.java
@@ -1,5 +1,7 @@
 package com.nishibu2003.m3u8_downloader.file;
 
+import java.io.UnsupportedEncodingException;
+
 public class FileUtil {
 	public static char[][] FORBIDDEN_CHAR = {
 			{'\\','￥'}, {'/','／'}, {':','：'},
@@ -23,5 +25,34 @@ public class FileUtil {
 		}
 		return new String(buf);
 	}
+	
+	public static String truncateForDir(String str, int len) {
+		
+		byte[] bytes = null;
+		try {
+			bytes = str.getBytes("Windows-31j");
+		} catch (UnsupportedEncodingException e) {
+			// Do nothing
+		}
 
+		if (bytes.length <= len) {
+			return str;
+		}
+
+		// 切り捨てたバイト列を格納する
+		byte[] truncated = new byte[len];
+		System.arraycopy(bytes, 0, truncated, 0, len);
+
+		// バイト列から文字列に戻す際、文字化けを防ぐために
+		// 末尾から順に1バイトずつ削って、正しくデコードできる位置を探す
+		for (int i = len; i >= 0; i--) {
+			try {
+				return new String(truncated, 0, i, "Windows-31j");
+			} catch (Exception e) {
+				// 無視して次へ
+			}
+		}
+
+		return ""; // すべて失敗した場合は空文字を返す
+	}
 }
